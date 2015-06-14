7d64111f13ce02b1bf19de298960e85e226940ea
Claude BUENO <claude.bueno@outlook.com>
2015-06-14 18:11:34 +0200

    update

--- header.php
+++ header.php
@@ -12,8 +12,6 @@
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Oswald">
-    <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Ubuntu">
     <link rel="profile" href="http://gmpg.org/xfn/11">
     <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
--- single.php
+++ single.php
@@ -9,22 +9,16 @@ get_header(); ?>
 
 	<div class="row">
 		<div class="col-md-8 content-area" role="main">
-
 		<?php while ( have_posts() ) : the_post(); ?>
-
 			<?php get_template_part( 'template-parts/content', 'single' ); ?>
-
 			<?php the_post_navigation(); ?>
-
 			<?php
 				// If comments are open or we have at least one comment, load up the comment template.
 				if ( comments_open() || get_comments_number() ) :
 					comments_template();
 				endif;
 			?>
-
 		<?php endwhile; // End of the loop. ?>
-
 		</div><!-- #main -->
 
 <?php get_sidebar(); ?>
