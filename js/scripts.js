8afe01bcd747ef64ecdebfa0da451b8ab3c05759
Claude BUENO <claude.bueno@outlook.com>
2016-01-30 13:09:16 +0100

    Add js

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@
 	License: GNU General Public License v2.0
 	License URI: http://www.gnu.org/licenses/gpl-2.0.html
 	
-	Copyright: (c) 2013 Theme Meme, http://themememe.com
+	Copyright: (c) 2013 Flat Natura, http://flatnatura.com
 */
 
 jQuery(document).ready(function($) {
