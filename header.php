d39714aa8c0168207d109668b68d16f5a4c4af94
Claude BUENO <claude.bueno@outlook.com>
2016-02-24 13:00:09 +0100

    Bio author & Similar posts

--- header.php
+++ header.php
@@ -21,7 +21,7 @@
 <body <?php body_class(); ?>>
     <?php do_action( 'before' ); ?>
 	<div class="site-top">
-		<div class="clearfix container">	
+		<div class="clearfix container">
 			<nav class="site-menu" role="navigation">
 				<div class="menu-toggle"><i class="fa fa-bars"></i></div>
 				<div class="menu-text"></div>
@@ -34,16 +34,16 @@
 						<?php get_search_form(); ?>
 					</div>
 				</div>
-			</div>		
+			</div>
 		</div>
-	</div>	
+	</div>
 
 	<header class="site-header" role="banner">
 		<div class="clearfix container">
 			<div class="site-branding">
 				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 				<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-			</div>	
+			</div>
 		</div><!-- .site-branding -->
 	</header><!-- #masthead -->
 
--- template-parts/content-single.php
+++ template-parts/content-single.php
@@ -25,7 +25,41 @@
 			) );
 		?>
 	</div><!-- .entry-content -->
-
+	<!-- Bio auteur -->
+	<div class="author-bio">
+		<div class="bio-avatar"><?php echo get_avatar(get_the_author_meta('user_email'),'128'); ?></div>
+		<p class="bio-name"><?php the_author_meta('display_name'); ?></p>
+		<p class="bio-desc"><?php the_author_meta('description'); ?></p>
+		<div class="clear"></div>
+	</div>
+		<!-- Similar items -->
+		<?php    
+		$tags = wp_get_post_tags($post->ID);
+		if ($tags) {
+			$tag_ids = array();
+			foreach($tags as $individual_tag) $tag_ids[] = $individual_tag->term_id;
+			$args=array(
+				'tag__in' => $tag_ids,
+				'post__not_in' => array($post->ID),
+				'showposts'=>6, // nombre d'articles à afficher
+				'caller_get_posts'=>1
+			);
+			$my_query = new wp_query($args);
+			if( $my_query->have_posts() ) {
+				echo '<h4>Articles similaires</h4><ul>';
+				while ($my_query->have_posts()) {
+					$my_query->the_post();
+				?>
+					<li>
+					  <a href="<?php the_permalink() ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+					</li>
+				<?php
+				}
+				echo '</ul><br>';
+			}
+			wp_reset_postdata();
+		}
+		?>
 	<footer class="entry-footer">
 		<?php flatnatura_entry_footer(); ?>
 	</footer><!-- .entry-footer -->
